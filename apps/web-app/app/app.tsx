433f609780ae2820a9cb96211e252116b1df871f
mostafa rahmani <mostafaa_rahmani@outlook.com>
2025-05-23 20:07:24 +0330

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/WE2NcWGlZI

---
+++ apps/web-app/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@nx-monorepo/web-app" />
+    </div>
+  );
+}
+
+export default App;
